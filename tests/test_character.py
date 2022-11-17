bb3086ad4fc4a1ad3704cd2bbfa4f6ede3c84d8f
Connie Aceves <acevesc@gmail.com>
2022-11-17 22:22:24 +0000

    Add Character class file

---
+++ tests/test_character.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from levelup.controller import GameController, DEFAULT_CHARACTER_NAME
+from levelup.character import Character
+
+
+class TestCharacter(TestCase):
+    expected_value: str
+    actual_value: str
+
+    def test_initialization(self):
+        testobj = Character("")
+        expected_value = ""
+        actual_value = testobj.name
+        self.assertNotEqual(actual_value, expected_value)
+
+    def test_EnterMap(self):
+        testobj 
