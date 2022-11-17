1446f7202a9acc4377ed23fe0d1698c6280e4253
Connie Aceves <acevesc@gmail.com>
2022-11-17 23:46:14 +0000

    daidluhaslifhasifh

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -4,3 +4,10 @@ from dataclasses import dataclass
 @dataclass
 class Character:
     name: str
+
+#    def __init__ (self, name):
+#        self.name = name
+#
+#    def getName():
+#        self.name = input("Please enter a name for your mighty warrior person")
+#        return self.name
