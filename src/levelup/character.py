a988214c748bbb93bf9bf2ceecf23f9f5485f7ae
Connie Aceves <acevesc@gmail.com>
2022-11-18 18:04:58 +0000

    updates to map.py, character.py

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,6 @@
 from levelup.position import Position
 from levelup.map import GameMap
+#from levelup.controller import Direction
 
 DEFAULT_POSITION = Position(0, 0)
 
@@ -10,9 +11,19 @@ class Character:
 
     def __init__(self, name: str):
         self.name = name
+   
+    def getName():
+        name = input("Enter Character Name")
+        return ("Nice to meet you brave warrior",(name),"!!")
 
     def enter_map(self, game_map: GameMap):
-        pass
+        m = int(input())
+        return m
+
+    def getPosition(): DEFAULT_POSITION
+        
+    #def setMap(m):
+        #pass
 
     def move(self):
         pass
--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,5 +1,14 @@
 from levelup.position import Position
 
+from enum import Enum
+
+
+class Direction(Enum):
+    NORTH = "n"
+    SOUTH = "s"
+    EAST = "e"
+    WEST = "w"
+
 
 class GameMap:
     startingPosition = Position(0,0)
